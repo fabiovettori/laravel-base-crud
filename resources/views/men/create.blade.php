4d93964023b195acfecc39ea50a14e8590d5f41f
Fabio Vettori <fabio_vettori@hotmail.it>
2021-01-21 17:28:07 +0100

    Form config for POST

--- resources/views/men/create.blade.php
+++ resources/views/men/create.blade.php
@@ -1,27 +1,29 @@
 @extends('layouts.app')
 
+@section('title', 'Create new product')
+
 @section('content')
     <section class="py-5" id="products-table">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <h3 class="pb-4">Create new product</h3>
-                    <form>
+                    <form action="{{ route('men.store') }}" method="POST">
                         <div class="form-group">
-                            <label for="exampleFormControlInput1">Name</label>
-                            <input type="text" class="form-control">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control">
 
-                            <label for="exampleFormControlInput1">Description</label>
-                            <input type="text" class="form-control">
+                            <label>Description</label>
+                            <textarea class="form-control" name="description" rows="3"></textarea>
 
-                            <label for="exampleFormControlInput1">Details</label>
-                            <input type="email" class="form-control">
+                            <label>Details</label>
+                            <input type="email" name="details" class="form-control">
 
-                            <label for="exampleFormControlInput1">Price</label>
-                            <input type="text" class="form-control">
+                            <label>Price</label>
+                            <input type="text" name="price" class="form-control">
 
-                            <label for="exampleFormControlInput1">Poster Image</label>
-                            <input type="file" class="form-control">
+                            <label>Poster Image</label>
+                            <input type="file" name="poster_image" class="form-control">
                         </div>
                     </form>
                 </div>
